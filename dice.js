f69eb6e4be93466da441418b341d6734004de256
theyogendra01 <yogendraswami297@gmail.com>
2025-01-21 10:21:04 +0530

    Updated file

--- dice.js
+++ dice.js
@@ -1,7 +1,53 @@
+const buttonEl = document.getElementById("roll-button");
+
+const diceEl = document.getElementById("dice");
+
+const rollHistoryEl = document.getElementById("roll-history");
+
+let historyList = [];
+
 function rollDice() {
-    const dice = document.getElementById('dice');
-    const randomNum = Math.floor(Math.random() * 6) + 1;
-    dice.textContent = `🎲 ${randomNum}`;
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+  diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+  updateRollHistory();
 }
 
-document.querySelector('button').addEventListener('click', rollDice);
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}</span>`;
+    rollHistoryEl.appendChild(listItem);
+  }
+}
+
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
+
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+  setTimeout(() => {
+    diceEl.classList.remove("roll-animation");
+    rollDice();
+  }, 1000);
+});
